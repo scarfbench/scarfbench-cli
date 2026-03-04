8d75eb419d61cd8dc77f9f71f84fcbc99d8c81e5
Rahul Krishna <i.m.ralk@gmail.com>
2026-03-03 22:55:35 -0500

    Update the validate subcommand to read the log file that is generated and classify the outcome in the metadata file
    Signed-off-by: Rahul Krishna <i.m.ralk@gmail.com>

--- .cargo/env.fish
+++
@@ -1,5 +0,0 @@
-# rustup shell setup
-if not contains "/home/rkrsn/workspace/scarfbench-org/scarf/.cargo/bin" $PATH
-    # Prepending path in case a system-installed rustc needs to be overridden
-    set -x PATH "/home/rkrsn/workspace/scarfbench-org/scarf/.cargo/bin" $PATH
-end
